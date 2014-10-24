2e36a4c0351ff22ead50d26dd98a55e2dd3e291c
Grantlyk <grant_leadbetter@live.co.uk>
2014-10-24 02:00:49 +0100

    Removed Comments

--- public/core.js
+++ public/core.js
@@ -3,7 +3,6 @@ var scotchTodo = angular.module('scotchTodo', []);
 function mainController($scope, $http) {
 	$scope.formData = {};
 
-	// when landing on the page, get all todos and show them
 	$http.get('/api/bugs')
 		.success(function(data) {
 			$scope.bugs = data;
@@ -12,11 +11,11 @@ function mainController($scope, $http) {
 			console.log('Error: ' + data);
 		});
 
-	// when submitting the add form, send the text to the node API
+
 	$scope.createBug = function() {
 		$http.post('/api/bugs', $scope.formData)
 			.success(function(data) {
-				$scope.formData = {}; // clear the form so our user is ready to enter another
+				$scope.formData = {}; 
 				$scope.bugs = data;
 				console.log(data);
 			})
@@ -25,7 +24,7 @@ function mainController($scope, $http) {
 			});
 	};
 
-	// delete a todo after checking it
+	
 	$scope.deleteBug = function(id) {
 		$http.delete('/api/bugs/' + id)
 			.success(function(data) {
