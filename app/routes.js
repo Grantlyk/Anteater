04a9848c1f47f36e26386f65dfe35284a1afa487
Grantlyk <grant_leadbetter@live.co.uk>
2014-10-24 02:01:52 +0100

    Test

--- app/routes.js
+++ app/routes.js
@@ -1,26 +1,22 @@
-var bug = require('./models/bug');
+ var bug = require('./models/bug');
 
 module.exports = function(app) {
 
 	// api ---------------------------------------------------------------------
-	// get all todos
 	app.get('/api/bugs', function(req, res) {
 
-		// use mongoose to get all todos in the database
+
 		bug.find(function(err, bugs) {
 
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err)
 
-			res.json(bugs); // return all todos in JSON format
+			res.json(bugs);
 		});
 	});
 
-	// create todo and send back all todos after creation
 	app.post('/api/bugs', function(req, res) {
 
-		// create a todo, information comes from AJAX request from Angular
 		bug.create({
 			desc : req.body.text,
 			fixed : false
@@ -28,7 +24,6 @@ module.exports = function(app) {
 			if (err)
 				res.send(err);
 
-			// get and return all the todos after you create another
 			bug.find(function(err, bugs) {
 				
 				if (err)
@@ -40,7 +35,6 @@ module.exports = function(app) {
 
 	});
 
-	// delete a todo
 	app.delete('/api/bugs/:bug_id', function(req, res) {
 		bug.remove({
 			_id : req.params.bug_id
@@ -48,7 +42,6 @@ module.exports = function(app) {
 			if (err)
 				res.send(err);
 
-			// get and return all the todos after you create another
 			bug.find(function(err, bugs) {
 				if (err)
 					res.send(err)
@@ -59,6 +52,6 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/index.html');
 	});
 };
